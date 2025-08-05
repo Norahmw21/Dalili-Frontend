38b08917c65d6007d5a9fe68fe384a762b90581e
Raghad <94093493+Raghadlh@users.noreply.github.com>
2025-08-05 15:25:57 +0300

    Create DoctorsList.vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 // src/main.js
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css'
 import App from './App.vue';
 import './style.css'
+import axios from 'axios';
+
+// Configure Axios globally
+axios.defaults.baseURL = 'http://localhost:8080'; // Your backend URL
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+
 import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App);
@@ -12,5 +21,8 @@ app.use(PrimeVue, {
         preset: Aura,
     },
 });
+
+
+app.config.globalProperties.$axios = axios;
 
 app.mount('#app');
