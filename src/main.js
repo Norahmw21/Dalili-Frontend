9a551a259eeef88a767ddefcd79ade2b049f0a99
Raghad <94093493+Raghadlh@users.noreply.github.com>
2025-08-05 10:34:32 +0300

    install prime

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
+// src/main.js
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
 import './style.css'
-import App from './App.vue'
+import Aura from '@primeuix/themes/aura';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    },
+});
+
+app.mount('#app');
