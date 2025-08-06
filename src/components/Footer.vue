e54f8a62fd71fae278b2fdc973f2ea30010be487
Rubabdran <rtbdrani@gmail.com>
2025-08-06 03:26:19 -0700

    add footer and navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+    <template>
+    <!-- Footer -->
+    <footer class="bg-white py-8">          
+        <div class="mt-8 pt-8 border-t text-center text-gray-500 text-sm">
+          © 2025 Dalili. All rights reserved.
+        </div>
+    </footer>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="bg-white shadow-md px-8 py-4 flex justify-between items-center">
+    <!-- Logo & Brand -->
+    <div class="flex items-center space-x-3">
+      <img src="../assets/logo.png" alt="Dalili Logo" class="w-15 h-15" />
+    </div>
+
+    <!-- Navigation Links -->
+    <ul class="flex space-x-6 text-gray-700 font-medium">
+      <li>
+        <a href="/src/views/Landing02.vue" class="hover:text-teal-600 transition duration-300 border-b-2 border-transparent hover:border-teal-500">Home</a>
+      </li>
+      <li>
+        <a href="#" class="hover:text-teal-600 transition duration-300 border-b-2 border-transparent hover:border-teal-500">About Us</a>
+      </li>
+      <li>
+        <a href="#" class="hover:text-teal-600 transition duration-300 border-b-2 border-transparent hover:border-teal-500">Find a Doctor</a>
+      </li>
+      <li>
+        <a href="#" class="hover:text-teal-600 transition duration-300 border-b-2 border-transparent hover:border-teal-500">Apps</a>
+      </li>
+      <li>
+        <a href="#" class="hover:text-teal-600 transition duration-300 border-b-2 border-transparent hover:border-teal-500">Contact</a>
+      </li>
+    </ul>
+
+
+  </nav>
+</template>
